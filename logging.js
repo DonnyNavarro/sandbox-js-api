5d9725f79def414452bc3cb4c66d8f5c981cb82b
Donny Navarro <donny@ultratesting.us>
2021-09-10 17:38:23 -0400

    Update logging.js

--- logging.js
+++ logging.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 exports.saveLog = function (filePrefix, data) {
-    // Save response locally as a unique file
-    const filenameBase = './logs/';
+    // Save data locally as a unique file
+    const folder = './logs/';
     const timestamp = new Date().toISOString();
     const filenameEnding = '.json';
-    fs.writeFile(filenameBase + filePrefix + '_' + timestamp + filenameEnding, data, 'utf8', () => {});
+    fs.writeFile(folder + filePrefix + '_' + timestamp + filenameEnding, data, 'utf8', () => {});
 }
