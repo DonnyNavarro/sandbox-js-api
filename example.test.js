7d68107e837a083e53ff4630f274e2c59db03147
Donny Navarro <donny@ultratesting.us>
2021-09-10 15:26:22 -0400

    setting up jest for test example

--- example.test.js
+++ example.test.js
@@ -7,6 +7,9 @@ async function exampleHttps() {
     const host = 'covid19-api.com';
     const path = '/country/all?format=json';
     var response = await api_https.apiRequest(method, host, path);
-    console.log(response);
+    expect(response.status).toEqual(200);
+    // console.log(response);
 }
-exampleHttps();
+test('testExample', () => {
+    exampleHttps();
+})
